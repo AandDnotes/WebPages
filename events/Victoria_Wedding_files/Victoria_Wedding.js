cdf3b594ac7bd278578da3ee386e6f1c6cdd4544
AandDnotes <38796630+AandDnotes@users.noreply.github.com>
2018-05-03 20:39:25 +0100

    Add files via upload

---
+++ events/Victoria_Wedding_files/Victoria_Wedding.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20120421
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,-1,1,202),url:'Victoria_Wedding_files/stroke.png'},{rect:new IWRect(-2,-2,1,1),url:'Victoria_Wedding_files/stroke_1.png'},{rect:new IWRect(-1,-2,302,1),url:'Victoria_Wedding_files/stroke_2.png'},{rect:new IWRect(301,-2,1,1),url:'Victoria_Wedding_files/stroke_3.png'},{rect:new IWRect(301,-1,1,202),url:'Victoria_Wedding_files/stroke_4.png'},{rect:new IWRect(301,201,1,1),url:'Victoria_Wedding_files/stroke_5.png'},{rect:new IWRect(-1,201,302,1),url:'Victoria_Wedding_files/stroke_6.png'},{rect:new IWRect(-2,201,1,1),url:'Victoria_Wedding_files/stroke_7.png'}],new IWSize(300,200))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Victoria_Wedding_files/Victoria_WeddingMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
