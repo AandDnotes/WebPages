a2b79f02621463468a7366332dbf01b7863867c2
AandDnotes <38796630+AandDnotes@users.noreply.github.com>
2018-05-03 20:37:55 +0100

    Add files via upload

---
+++ events/Paul_Wedding_files/Paul_Wedding.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20120421
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,-1,1,227),url:'Paul_Wedding_files/stroke.png'},{rect:new IWRect(-2,-2,1,1),url:'Paul_Wedding_files/stroke_1.png'},{rect:new IWRect(-1,-2,302,1),url:'Paul_Wedding_files/stroke_2.png'},{rect:new IWRect(301,-2,1,1),url:'Paul_Wedding_files/stroke_3.png'},{rect:new IWRect(301,-1,1,227),url:'Paul_Wedding_files/stroke_4.png'},{rect:new IWRect(301,226,1,1),url:'Paul_Wedding_files/stroke_5.png'},{rect:new IWRect(-1,226,302,1),url:'Paul_Wedding_files/stroke_6.png'},{rect:new IWRect(-2,226,1,1),url:'Paul_Wedding_files/stroke_7.png'}],new IWSize(300,225))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Paul_Wedding_files/Paul_WeddingMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
