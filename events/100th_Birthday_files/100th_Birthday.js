d4584fcb7968fc5e857909c341a29ab328a280e2
AandDnotes <38796630+AandDnotes@users.noreply.github.com>
2018-05-03 20:31:28 +0100

    Add files via upload

---
+++ events/100th_Birthday_files/100th_Birthday.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20120421
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('100th_Birthday_files/100th_BirthdayMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');IMpreload('100th_Birthday_files','shapeimage_2','0');fixupIECSS3Opacity('id3');applyEffects()}
