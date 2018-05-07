3ed180119a8d82ad4cb923735baba6163b1f90ad
AandDnotes <38796630+AandDnotes@users.noreply.github.com>
2018-05-07 17:24:28 +0100

    Add files via upload

---
+++ trips/USA2006/USA_2006/USA_2006_files/USA_2006.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20101128
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_4:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_3:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_2:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),stroke_0:new IWPhotoFrame([IWCreateImage('USA_2006_files/Modern_C_TL.png'),IWCreateImage('USA_2006_files/Modern_S_T.png'),IWCreateImage('USA_2006_files/Modern_C_TR.png'),IWCreateImage('USA_2006_files/Modern_S_R.png'),IWCreateImage('USA_2006_files/Modern_C_BR.png'),IWCreateImage('USA_2006_files/Modern_S_B.png'),IWCreateImage('USA_2006_files/Modern_C_BL.png'),IWCreateImage('USA_2006_files/Modern_S_L.png')],null,2,0.700000,0.000000,0.000000,0.000000,0.000000,17.000000,17.000000,17.000000,20.000000,40.000000,40.000000,40.000000,240.000000,null,null,null,0.100000),shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_5:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('USA_2006_files/USA_2006Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
