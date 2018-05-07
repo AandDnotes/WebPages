4671324f6aa8cfc4860100a252ca47db73c45e6c
AandDnotes <38796630+AandDnotes@users.noreply.github.com>
2018-05-07 17:18:28 +0100

    Add files via upload

---
+++ trips/USA2006/USA_2006/BearMountain_files/BearMountain.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20101128
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_3:new IWShadow({blurRadius:7,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_1:new IWShadow({blurRadius:7,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_4:new IWShadow({blurRadius:7,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_5:new IWShadow({blurRadius:7,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_6:new IWShadow({blurRadius:7,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:7,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_2:new IWShadow({blurRadius:7,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('BearMountain_files/BearMountainMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');detectBrowser();adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');fixAllIEPNGs('Media/transparent.gif');Widget.onload();IMpreload('BearMountain_files','shapeimage_3','0');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
