e7c56493a4df63a7826e7197f3cc09ca0b5b6cac
AandDnotes <38796630+AandDnotes@users.noreply.github.com>
2018-05-07 17:12:18 +0100

    Add files via upload

---
+++ trips/Trips2009/Trips_2009/Uppark_files/Uppark.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20101128
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Uppark_files/UpparkMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
