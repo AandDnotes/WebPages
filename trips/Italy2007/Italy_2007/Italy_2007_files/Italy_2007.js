88b1924b106132d75b448109198d23b737e78f4a
AandDnotes <38796630+AandDnotes@users.noreply.github.com>
2018-05-07 09:38:35 +0100

    Add files via upload

---
+++ trips/Italy2007/Italy_2007/Italy_2007_files/Italy_2007.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20101128
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Italy_2007_files/Modern_C_TL.png'),IWCreateImage('Italy_2007_files/Modern_S_T.png'),IWCreateImage('Italy_2007_files/Modern_C_TR.png'),IWCreateImage('Italy_2007_files/Modern_S_R.png'),IWCreateImage('Italy_2007_files/Modern_C_BR.png'),IWCreateImage('Italy_2007_files/Modern_S_B.png'),IWCreateImage('Italy_2007_files/Modern_C_BL.png'),IWCreateImage('Italy_2007_files/Modern_S_L.png')],null,2,0.700000,0.000000,0.000000,0.000000,0.000000,17.000000,17.000000,17.000000,20.000000,40.000000,40.000000,40.000000,240.000000,null,null,null,0.100000),stroke_1:new IWPhotoFrame([IWCreateImage('Italy_2007_files/Modern_C_TL.png'),IWCreateImage('Italy_2007_files/Modern_S_T.png'),IWCreateImage('Italy_2007_files/Modern_C_TR.png'),IWCreateImage('Italy_2007_files/Modern_S_R.png'),IWCreateImage('Italy_2007_files/Modern_C_BR.png'),IWCreateImage('Italy_2007_files/Modern_S_B.png'),IWCreateImage('Italy_2007_files/Modern_C_BL.png'),IWCreateImage('Italy_2007_files/Modern_S_L.png')],null,2,0.700000,0.000000,0.000000,0.000000,0.000000,17.000000,17.000000,17.000000,20.000000,40.000000,40.000000,40.000000,240.000000,null,null,null,0.100000),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Italy_2007_files/Italy_2007Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
