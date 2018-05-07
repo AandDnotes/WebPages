d096581f6ba4c7bfef2dc9415fef8e1c2d46f9d2
AandDnotes <38796630+AandDnotes@users.noreply.github.com>
2018-05-07 17:05:37 +0100

    Add files via upload

---
+++ trips/Trips2009/Trips_2009/Day_Trips_2009_files/Day_Trips_2009.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20101128
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Day_Trips_2009_files/Day_Trips_2009Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
